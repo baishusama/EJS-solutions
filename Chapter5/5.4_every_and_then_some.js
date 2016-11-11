1c50a7eaa0e429a671980f52a22362dfd606ca5c
dingxinyi <baishusama@outlook.com>
2016-11-11 19:42:50 +0800

    exercise4 in chapter5 is add.

---
+++ Chapter5/5.4_every_and_then_some.js
@@ -0,0 +1,13 @@
+function every(arr, test){
+    for(var i = 0; i < arr.length; i++){
+        if(!test(arr[i])) return false;
+    }
+    return true;
+}
+
+function some(arr, test){
+    for(var i = 0; i < arr.length; i++){
+        if(test(arr[i])) return true;
+    }
+    return false;
+}
