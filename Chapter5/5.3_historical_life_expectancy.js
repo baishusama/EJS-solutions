777c67597e8fe0d18865483ff6f76412d89c3643
dingxinyi <baishusama@outlook.com>
2016-11-14 00:39:54 +0800

    exercises in chapter5 are finally finished.

--- Chapter5/5.3_historical_life_expectancy.js
+++ Chapter5/5.3_historical_life_expectancy.js
@@ -24,7 +24,7 @@ ancestry.forEach(function(person){
     byName[person.name] = person;
 });*/
 
-// My code:
+// My codes after reading <<EJS>>'s hint:
 
 function group(people){
     var res = {};
@@ -38,11 +38,56 @@ function group(people){
     return res;
 }
 
-function groupBy(ancestry, group){
-    var detailList = group(ancestry);
-    var averageList = {};
-    for(var cent in detailList){
-        averageList[cent] = average(detailList[cent]);
-    }
-    return averageList;
+// // I misunderstood groupBy at first
+// function groupBy(ancestry, group){
+//     var detailList = group(ancestry);
+//     var averageList = {};
+//     for(var cent in detailList){
+//         averageList[cent] = average(detailList[cent]);
+//     }
+//     return averageList;
+// }
+
+// to print the average ages for individual centuries
+var centuries = group(ancestry);
+for(var cent in centuries){
+    console.log(cent + ": " + average(centuries[cent]));
 }
+
+// abstract the grouping operation
+function groupBy(arr, func){
+    var groups = {};
+    arr.forEach(function(element){
+        var groupName = func(element);
+        if(!(groupName in groups)){
+            groups[groupName] = [];
+        }
+        groups[groupName].push(element);
+    });
+    return groups;
+}
+
+// <<EJS>>'s solution:
+function groupBy(array, groupOf) {
+  var groups = {};
+  array.forEach(function(element) {
+    var groupName = groupOf(element);
+    if (groupName in groups)
+      groups[groupName].push(element);
+    else
+      groups[groupName] = [element];
+  });
+  return groups;
+}
+
+var byCentury = groupBy(ancestry, function(person) {
+  return Math.ceil(person.died / 100);
+});
+
+for (var century in byCentury) {
+  var ages = byCentury[century].map(function(person) {
+    return person.died - person.born;
+  });
+  console.log(century + ": " + average(ages));
+}
+
--- Chapter5/5.4_every_and_then_some.js
+++ Chapter5/5.4_every_and_then_some.js
@@ -1,3 +1,4 @@
+// <<EJS>>'s solution is almost same as mine:
 function every(arr, test){
     for(var i = 0; i < arr.length; i++){
         if(!test(arr[i])) return false;
