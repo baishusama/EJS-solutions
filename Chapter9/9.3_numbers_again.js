bec1f747a3343320076bade18e42d00cbc3e3a00
dingxinyi <baishusama@outlook.com>
2016-11-17 23:01:56 +0800

    correct exercise3 in chapter9.

--- Chapter9/9.3_numbers_again.js
+++ Chapter9/9.3_numbers_again.js
@@ -1,2 +1,13 @@
-var jsStyleNumber = /^[+-]?(((\d+)|(\.\d+)|(\d+\.)|(\d+\.\d+))+([eE][+-]?\d+)?)$/;
-//???
+var jsStyleNumber_MySolution = /^[+-]?(\d+|\.\d+|\d+\.|\d+\.\d+)([eE][+-]?\d+)?$/;
+
+// <<EJS>>'s hint :
+// Matching the optional sign in front of the number, 
+// as well as in front of the exponent, 
+// can be done with [+\-]? or (\+|-|) (plus, minus, or nothing).
+
+var jsStyleNumber_Hint1 = /^[+\-]?(\d+\.\d*|\.?\d+)([eE][+\-]?\d+)?$/;
+var jsStyleNumber_Hint2 = /^[+\-]?(\d+\.?\d*|\.\d+)([eE][+\-]?\d+)?$/;
+
+// <<EJS>>'s solution:
+
+var jsStyleNumber_EjsSolution = /^(\+|-|)(\d+(\.\d*)?|\.\d+)([eE](\+|-|)\d+)?$/;
